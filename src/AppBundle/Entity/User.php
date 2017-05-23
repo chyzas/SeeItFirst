dfade559b9bcd5ccf27561bbe569e96bd44bfef5
Mantas <chyzas@gmail.com>
2017-05-23 18:53:47 +0300

    fixed registration bug, default value for availableFilterCount was not set

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -37,7 +37,7 @@ class User extends BaseUser
     protected $email;
 
     /** @ORM\Column(name="available_filter_count", type="integer") */
-    protected $availableFilterCount;
+    protected $availableFilterCount = 2;
 
     /**
      * @var string
