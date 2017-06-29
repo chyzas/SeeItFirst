7df09cbb4ffac7f3004803aacbc3ee9a37dd92c0
mjonusas <chyzas@gmail.com>
2017-06-29 10:25:22 +0200

    fixtures

--- src/AppBundle/DataFixtures/ORM/LoadSitesData.php
+++ src/AppBundle/DataFixtures/ORM/LoadSitesData.php
@@ -1,7 +1,34 @@
 <?php
 
 
-class LoadSitesData
+use AppBundle\Entity\Site;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadSitesData implements FixtureInterface
 {
 
+    /**
+     * @inheritdoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $skelbiu = new Site();
+        $skelbiu->setName('skelbiu');
+        $skelbiu->setSiteUrl('skelbiu.lt');
+        $manager->persist($skelbiu);
+        $manager->flush();
+
+        $autoplius = new Site();
+        $autoplius->setName('autoplius');
+        $autoplius->setSiteUrl('autoplius.lt');
+        $manager->persist($autoplius);
+        $manager->flush();
+
+        $aruodas = new Site();
+        $aruodas->setName('aruodas');
+        $aruodas->setSiteUrl('aruodas.lt');
+        $manager->persist($aruodas);
+        $manager->flush();
+    }
 }
