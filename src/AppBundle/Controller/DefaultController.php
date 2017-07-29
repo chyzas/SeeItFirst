890c68971eb5565ec229ab4dc15a365d79a48166
mjonusas <chyzas@gmail.com>
2017-07-29 17:10:14 +0300

    ajax and form validation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,71 +4,81 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Site;
 use AppBundle\Form\FirstQueryType;
-use AppBundle\Services\Queue\Message;
 use GuzzleHttp\Exception\ClientException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-
-    public function testAction()
+    /**
+     * @return Response
+     */
+    public function indexAction()
     {
-        $queue = $this->get('app.sqs_queue');
-            /** @var Message $message */
-        $message = $queue->receive();
-        $body = $message->getBody();
-        return $this->render('@App/Emails/results.html.twig', ['data' => $body->getData()]);
-    }
-
-
-    public function indexAction(Request $request): Response
-    {
-        $form = $this->createForm(new FirstQueryType());
-
-        $form->handleRequest($request);
-
-        if (!($form->isSubmitted() && $form->isValid())) {
-
-            return $this->render('AppBundle:default:index.html.twig', [
-                'form' => $form->createView(),
-                'urls' => $this->getAvailableUrls()
-            ]);
-        }
-
-        $userManager = $this->container->get('fos_user.user_manager');
-        $formData = $form->getData();
-        $email = $formData['email'];
-
-        $user = $userManager->findUserBy(['email' => $email]);
-        if (!$user) {
-            $user = $this->get('user_service')->createUser($email);
-        }
-
-        $em = $this->get('doctrine.orm.entity_manager');
-        $em->getConnection()->beginTransaction();
-        try {
-            $filterManager = $this->get('filter_manager');
-            $filterManager->addFilter($user, $formData['url'], $formData['name']);
-            $em->getConnection()->commit();
-
-            $this->addFlash('success', $this->get('translator')->trans('registration.flash.confirm_filter', ['%email%' => $user->getEmail()]));
-
-
-        } catch (ClientException $e) {
-            $em->getConnection()->rollBack();
-            $this->addFlash('danger', $this->get('translator')->trans('errors.bad_request'));
-        }
-        catch (\Exception $e) {
-            $em->getConnection()->rollBack();
-            $this->addFlash('danger', $e->getMessage());
-        }
-
         return $this->render('AppBundle:default:index.html.twig', [
             'form' => $this->createForm(new FirstQueryType())->createView(),
             'urls' => $this->getAvailableUrls()
         ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function submitAction(Request $request)
+    {
+        if ($request->isXmlHttpRequest() && $request->getMethod() == Request::METHOD_POST) {
+            $userManager = $this->container->get('fos_user.user_manager');
+            $formData = $request->request->all();
+
+            $email = $formData['email'];
+            $url = $formData['url'];
+            $name = $formData['name'];
+
+            if (!($email && $url && $name)) {
+                return new JsonResponse([
+                    'message' => $this->get('translator')->trans('errors.fill_missing_fields')
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            $user = $userManager->findUserBy(['email' => $email]);
+            if (!$user) {
+                $user = $this->get('user_service')->createUser($email);
+            }
+
+            $em = $this->get('doctrine.orm.entity_manager');
+            $em->getConnection()->beginTransaction();
+            try {
+                $filterManager = $this->get('filter_manager');
+                $filterManager->addFilter($user, $url, $name);
+                $em->getConnection()->commit();
+
+                return new JsonResponse([
+                    'message' => $this->get('translator')->trans('registration.flash.confirm_filter', ['%email%' => $user->getEmail()])
+                ]);
+
+            } catch (ClientException $e) {
+                $em->getConnection()->rollBack();
+
+                return new JsonResponse([
+                        'message' => $this->get('translator')->trans('errors.bad_request')
+                    ], Response::HTTP_BAD_REQUEST
+                );
+            }
+            catch (\Exception $e) {
+                $em->getConnection()->rollBack();
+
+                return new JsonResponse([
+                    'message' => $e->getMessage()
+                ], Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        return new JsonResponse([
+            'message' => $this->get('translator')->trans('errors.something_went_wrong')
+        ], Response::HTTP_BAD_REQUEST);
     }
 
     /**
--- web/js/form-validation.js
+++ web/js/form-validation.js
@@ -4,16 +4,17 @@ $(function() {
             'first_query[url]': "required",
             'first_query[name]': "required",
             'first_query[email]': {
+                required: true,
                 email: true
             }
         },
         messages: {
             'first_query[url]': "Įveskite užklausos nuorodą",
             'first_query[name]': "Įveskite pavadinimą",
-            'first_query[email]': "Blogas elektroninio pašto formatas"
-        },
-        submitHandler: function(form) {
-            form.submit();
+            'first_query[email]': {
+                required: 'Įveskite E-mail',
+                email: "Blogas elektroninio pašto formatas"
+            }
         }
     });
 });
